2b0c42ff58fb72c544513a1efe5150a6b1a41abf
duq <duq@eastcoast.bean.com>
2017-08-02 11:48:24 -0400

    code cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient
 var async = require('async');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     get_data_list( function(result) {
         res.render('index', {data:result});
@@ -16,10 +16,8 @@ router.get('/detail/:id', function(req, res, next) {
 });
 
 function get_data_list(callback){
-    var MongoClient = require('mongodb').MongoClient
     var url =g_mongo_url;
     MongoClient.connect(url, function(err, db) {
-        console.log("Connected correctly to server");
         var collection = db.collection('Titles');
         collection.find({}).toArray(function(err, docs) {
             callback(docs);
@@ -29,10 +27,8 @@ function get_data_list(callback){
 }
 
 function get_data_by_title_id(title_id,callback){
-    var MongoClient = require('mongodb').MongoClient
     var url =g_mongo_url;
     MongoClient.connect(url, function(err, db) {
-        console.log("Connected correctly to server");
         var collection = db.collection('Titles');
         collection.find({TitleId:parseInt(title_id)}).toArray(function(err, docs) {
             callback(docs);
